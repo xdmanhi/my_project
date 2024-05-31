322d3161a37e579db6b6f117006c77e215bc0a8d
xdmanhi <xdman23134@gmail.com>
2024-05-31 22:17:24 +0200

    new project

---
+++ router.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from repository import TaskRepository
+from schemas import STaskAdd, STask
+
+router = APIRouter(
+    prefix='/tasks',
+    tags=['Tasks'],
+)
+
+@router.post('')
+async def add_task(
+        task: Annotated[STaskAdd, Depends()]
+):
+    task_id = await TaskRepository.add_one(task)
+    return {'ok': True, 'task_id': task_id}
+
+@router.get('')
+async def get_tasks() -> STask:
+    tasks = await TaskRepository.find_all()
+    return tasks
